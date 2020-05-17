749f4cab97703e464d3a585f26dc37a6ece63123
Ulysse Carion <ulysse@segment.com>
2020-05-17 15:41:03 -0700

    Choose crypto.Hash from algorithm value

--- dsig.go
+++ dsig.go
@@ -3,15 +3,11 @@ package dsig
 import (
 	"crypto"
 	"crypto/rsa"
-	"crypto/sha1"
-	"crypto/sha256"
 	"crypto/subtle"
 	"crypto/x509"
 	"encoding/base64"
-	"encoding/hex"
 	"encoding/xml"
 	"errors"
-	"fmt"
 
 	"github.com/ucarion/c14n"
 	"github.com/ucarion/dsig/internal/sigsplit"
@@ -24,28 +20,17 @@ type Signature struct {
 }
 
 func (s *Signature) Verify(cert *x509.Certificate, r c14n.RawTokenReader) error {
-	// if !strings.HasPrefix(s.SignedInfo.Reference.URI, "#") {
-	// 	return errors.New("not a fragment")
-	// }
-
 	toDigest, toVerify, err := sigsplit.SplitSignature(r)
 	if err != nil {
 		return err
 	}
-
-	// id := s.SignedInfo.Reference.URI[1:]
-	// tokenReader := omitSignatureReader{RawTokenReader: r, stack: &stack.Stack{}}
-	// payload, err := c14n.Canonicalize(id, &tokenReader)
-	// if err != nil {
-	// 	return err
-	// }
 
 	expectedDigest, err := base64.StdEncoding.DecodeString(s.SignedInfo.Reference.DigestValue)
 	if err != nil {
 		return err
 	}
 
-	h := sha1.New()
+	h := s.SignedInfo.Reference.DigestMethod.hash().New()
 	h.Write(toDigest)
 	if subtle.ConstantTimeCompare(expectedDigest, h.Sum(nil)) != 0 {
 		return errors.New("digest not correct")
@@ -56,7 +41,7 @@ func (s *Signature) Verify(cert *x509.Certificate, r c14n.RawTokenReader) error 
 		return errors.New("not rsa public key")
 	}
 
-	h = sha256.New()
+	h = s.SignedInfo.SignatureMethod.hash().New()
 	h.Write(toVerify)
 
 	expectedSignature, err := base64.StdEncoding.DecodeString(s.SignatureValue)
@@ -64,87 +49,12 @@ func (s *Signature) Verify(cert *x509.Certificate, r c14n.RawTokenReader) error 
 		return err
 	}
 
-	fmt.Println("ucarion", toVerify)
-	fmt.Println(hex.EncodeToString(expectedSignature))
-	fmt.Println(hex.EncodeToString(h.Sum(nil)))
-
-	fmt.Println(s.SignedInfo.SignatureMethod.Algorithm)
-	if err := rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, h.Sum(nil), expectedSignature); err != nil {
+	if err := rsa.VerifyPKCS1v15(publicKey, s.SignedInfo.SignatureMethod.hash(), h.Sum(nil), expectedSignature); err != nil {
 		return err
-	} else {
-		fmt.Println("RSA WORKED")
 	}
 
-	fmt.Println(string(toVerify))
 	return nil
 }
-
-// type omitSignatureReader struct {
-// 	depth       int  // the current depth in the node tree
-// 	inSignature bool // whether we are currently in the signature to skip
-// 	stack       *stack.Stack
-// 	c14n.RawTokenReader
-// }
-
-// func (r *omitSignatureReader) RawToken() (xml.Token, error) {
-// 	t, err := r.RawTokenReader.RawToken()
-// 	// fmt.Println("RAWTOKEN", t, err)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	switch t := t.(type) {
-// 	case xml.StartElement:
-// 		names := map[string]string{}
-// 		for _, attr := range t.Attr {
-// 			if attr.Name.Space == "xmlns" {
-// 				names[attr.Name.Local] = attr.Value
-// 			} else if attr.Name.Space == "" && attr.Name.Local == "xmlns" {
-// 				names[""] = attr.Value
-// 			}
-// 		}
-
-// 		r.stack.Push(names)
-
-// 		// fmt.Println("start", r.depth, r.stack.Get(t.Name.Space), t.Name.Local)
-// 		if r.depth == 1 && r.stack.Get(t.Name.Space) == "http://www.w3.org/2000/09/xmldsig#" && t.Name.Local == "Signature" {
-// 			// fmt.Println("found sig", r.depth)
-// 			r.inSignature = true
-// 		}
-
-// 		r.depth++
-
-// 		if r.inSignature {
-// 			return r.RawToken()
-// 		}
-
-// 		return t, nil
-// 	case xml.EndElement:
-// 		r.depth--
-// 		r.stack.Pop()
-
-// 		// fmt.Println("end", r.depth)
-
-// 		if r.depth == 1 && r.inSignature {
-// 			// fmt.Println("foudn end of sig")
-// 			r.inSignature = false
-// 			return r.RawToken()
-// 		}
-
-// 		if r.inSignature {
-// 			return r.RawToken()
-// 		}
-
-// 		// fmt.Println("return this end")
-// 		return t, nil
-// 	default:
-// 		if r.inSignature {
-// 			return r.RawToken()
-// 		}
-
-// 		return t, nil
-// 	}
-// }
 
 type SignedInfo struct {
 	XMLName                xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# SignedInfo"`
@@ -161,6 +71,17 @@ type CanonicalizationMethod struct {
 type SignatureMethod struct {
 	XMLName   xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# SignatureMethod"`
 	Algorithm string   `xml:"Algorithm,attr"`
+}
+
+func (s *SignatureMethod) hash() crypto.Hash {
+	switch s.Algorithm {
+	case "http://www.w3.org/2000/09/xmldsig#rsa-sha1":
+		return crypto.SHA1
+	case "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256":
+		return crypto.SHA256
+	default:
+		return 0
+	}
 }
 
 type Reference struct {
@@ -184,4 +105,15 @@ type Transform struct {
 type DigestMethod struct {
 	XMLName   xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# DigestMethod"`
 	Algorithm string   `xml:"Algorithm,attr"`
+}
+
+func (d *DigestMethod) hash() crypto.Hash {
+	switch d.Algorithm {
+	case "http://www.w3.org/2000/09/xmldsig#sha1":
+		return crypto.SHA1
+	case "http://www.w3.org/2001/04/xmlenc#sha256":
+		return crypto.SHA256
+	default:
+		return 0
+	}
 }
